4fbb767bde930709eb1e238aedfd689dd000e248
Szymon Orchowski <szymonorchowski@gmail.com>
2022-05-23 17:50:43 +0100

    Initial commit

---
+++ src/Presentation/Camera.js
@@ -0,0 +1,43 @@
+import * as THREE from 'three'
+import Presentation from './Presentation'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+
+export default class Camera
+{
+    constructor()
+    {
+        this.presentation = new Presentation()
+        this.sizes = this.presentation.sizes
+        this.scene = this.presentation.scene
+        this.canvas = this.presentation.canvas
+
+        this.setInstance()
+        this.setControls()
+    }
+
+    setInstance()
+    {
+        this.instance = new THREE.PerspectiveCamera(35, this.sizes.width / this.sizes.height, 0.1, 100)
+        this.instance.position.set(-4, 0.2, 0)
+        this.instance.zoom = 1
+        this.instance.updateProjectionMatrix()
+        this.scene.add(this.instance)
+    }
+
+    setControls()
+    {
+        this.controls = new OrbitControls(this.instance, this.canvas)
+        this.controls.enableDamping = true
+    }
+
+    resize()
+    {
+        this.instance.aspect = this.sizes.width / this.sizes.height
+        this.instance.updateProjectionMatrix()
+    }
+
+    update()
+    {
+        this.controls.update()
+    }
+}
